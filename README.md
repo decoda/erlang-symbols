d83980154e1579151f997778959a2e5f5ebafcce
tangjing <enkotangjing@163.com>
2018-03-20 14:47:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# VS Code Erlang symbols
+
+Search functions in erlang files.
+
+## Features
+
+* Search erlang functions
+
+## Requirements
+
+## Release Notes
+
+### 0.0.1
+
+Initial release
---
+++ src/extension.ts
@@ -0,0 +1,20 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import {
+    ExtensionContext,
+    languages
+} from 'vscode';
+import ElrDocumentSymbolProvider from './erl_document_symbol_provider';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: ExtensionContext) {
+    context.subscriptions.push(languages.registerDocumentSymbolProvider(
+        { language: "erlang" }, new ElrDocumentSymbolProvider()
+    ));    
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
