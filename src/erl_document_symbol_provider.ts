bc0d2f2f28362524572d7ab5beefb22ffab9d70c
decoda <enkotangjing@163.com>
2018-10-22 18:03:06 +0800

    update function search

--- src/erl_document_symbol_provider.ts
+++ src/erl_document_symbol_provider.ts
@@ -11,12 +11,18 @@ import {
 export default class ElrDocumentSymbolProvider implements DocumentSymbolProvider {
   private readonly comment_regex = /^\s*%/;
   private readonly function_regex = /^((\w+)\(.*\))\s*(when\s+.*)?\->/;
+  private readonly funcline_regex = /^(\w+)\(.*,\s*$/;
   private readonly macro_regex = /^\s*\-define\((\w+),/;
   private readonly record_regex = /^\s*\-record\((\w+),/;
   public provideDocumentSymbols(document: TextDocument, token: CancellationToken): Thenable<SymbolInformation[]> {
     return new Promise((resolve, reject) => {
-      let symbols = [];
+      let symbols: {
+        name: string,
+        kind: SymbolKind,
+        location: Location,
+      }[] = [];
 
+      let last_line = "";
       for (var i = 0; i < document.lineCount; i++) {
         const line = document.lineAt(i);
         if (!line.text) {
@@ -28,51 +34,50 @@ export default class ElrDocumentSymbolProvider implements DocumentSymbolProvider
         }
 
         const map: {[key:string]: number} = {};
-        match = line.text.match(this.function_regex);
-        if (match !== null) {
-          const label = match[1].trim();
-          if (map[label] == 1) {
-            continue;
-          }
-          map[label] = 1;
-          symbols.push({
-            name: label,
-            kind: SymbolKind.Function,
-            location: new Location(document.uri, line.range)
-          })
+        if (this.check_symbol_regx(document, line, symbols, map, this.function_regex, SymbolKind.Function))
+          continue;
+
+        if (this.check_symbol_regx(document, line, symbols, map, this.macro_regex, SymbolKind.Constant))
+          continue;
+
+        if (this.check_symbol_regx(document, line, symbols, map, this.record_regex, SymbolKind.Struct))
+          continue;
+
+        if (last_line !== "") {
+          let lst = last_line.replace(/[\r\n]/g, '');
+          let cur = line.text.replace(/(^\s*)/g, '');
+          last_line = "";
+          this.check_symbol_regx(document, line, symbols, map, this.function_regex, SymbolKind.Function, lst+cur);
           continue;
         }
-        match = line.text.match(this.macro_regex);
-        if (match !== null) {
-          const label = match[1].trim();
-          if (map[label] == 2) {
-            continue;
-          }
-          map[label] = 2;
-          symbols.push({
-            name: label,
-            kind: SymbolKind.Constant,
-            location: new Location(document.uri, line.range)
-          })
-          continue;
-        }
-        match = line.text.match(this.record_regex);
-        if (match !== null) {
-          const label = match[1].trim();
-          if (map[label] == 3) {
-            continue;
-          }
-          map[label] = 3;
-          symbols.push({
-            name: label,
-            kind: SymbolKind.Struct,
-            location: new Location(document.uri, line.range)
-          })
-          continue;
-        }
+
+        match = line.text.match(this.funcline_regex);
+        if (match !== null)
+          last_line = line.text
       }
 
       resolve(<any>symbols);
     });
+  }
+
+  private check_symbol_regx(document:any, line:any, symbols:any[], map:any, regx:any, kind:any, text:any=null): boolean {
+    if (text == null) {
+        text = line.text;
+    }
+    let match = text.match(regx);
+    if (match !== null) {
+      const label = match[1].trim();
+      if (map[label] == kind) {
+          return true;
+      }
+      map[label] = kind;
+      symbols.push({
+        name: label,
+        kind: kind,
+        location: new Location(document.uri, line.range)
+      })
+      return true;
+    }
+    return false;
   }
 }
